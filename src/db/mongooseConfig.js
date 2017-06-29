e424d21d5c303e64bf474f0860151329ccfbde59
Endre Vegh <endre.vegh@yahoo.com>
2017-06-29 16:22:09 +0100

    heroku

--- src/db/mongooseConfig.js
+++ src/db/mongooseConfig.js
@@ -5,7 +5,7 @@ const environment = process.env.NODE_ENV;
 if (environment === "TEST") mongoose.connect("mongodb://localhost:27017/test");
 else if (environment === "DEVELOPMENT")
   mongoose.connect("mongodb://localhost:27017/development");
-else if (environment === "PRODUCTION" || environment === 'production')
+else
   mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.Promise = global.Promise;
